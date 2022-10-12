2510758129a6dc97f9f764d204cb8b9ffe62c73e
Michael Bynum <michaelbynum@users.noreply.github.com>
2022-10-12 07:51:19 -0600

    updating license headers

--- src/docs/source/conf.py
+++ src/docs/source/conf.py
@@ -1,14 +1,3 @@
-#  ___________________________________________________________________________
-#
-#  Parapint
-#  Copyright (c) 2020
-#  National Technology and Engineering Solutions of Sandia, LLC
-#  Under the terms of Contract DE-NA0003525 with National Technology and
-#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
-#  rights in this software.
-#  This software is distributed under the 3-clause BSD License.
-#  ___________________________________________________________________________
-
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
